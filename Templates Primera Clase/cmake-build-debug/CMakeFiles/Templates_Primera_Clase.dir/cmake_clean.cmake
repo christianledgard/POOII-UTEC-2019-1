0f72addc605da307ee465f45595af0112d416383
Christian Ledgard <christianledgard@gmail.com>
2019-05-04 09:46:41 -0500

    Actualizando

---
+++ Templates Primera Clase/cmake-build-debug/CMakeFiles/Templates_Primera_Clase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Templates_Primera_Clase.dir/Ejemplo_Simple.cpp.o"
+  "Templates_Primera_Clase.pdb"
+  "Templates_Primera_Clase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Templates_Primera_Clase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
